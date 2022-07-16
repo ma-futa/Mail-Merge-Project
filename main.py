f0696e25749ff290272dd6c6fc238b4826785664
mohammed <mohammedfuta@gmail.com>
2022-07-16 11:07:27 +0000

    small optimization

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
-# TODO: Create a letter using starting_letter.txt
-# for each name in invited_names.txt
-# Replace the [name] placeholder with the actual name.
-# Save the letters in the folder "ReadyToSend".
-
-# Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-# Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-# Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
-
-
-starting_file = ''
 with open('./Input/Letters/starting_letter.txt') as letter_data:
     starting_file = letter_data.read()
     with open('./Input/Names/invited_names.txt') as names_data:
@@ -16,6 +5,3 @@ with open('./Input/Letters/starting_letter.txt') as letter_data:
             name = name.replace('\n', '')
             with open(f'./Output/ReadyToSend/{name}_invitation.txt', mode='w') as invitation:
                 invitation.write(starting_file.replace('[name]', name))
-
-
-
